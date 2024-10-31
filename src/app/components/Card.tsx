b35ba3f12928a34a36f3120a757bdea6db9c7cec
mhf29695 <mhf29695@uga.edu>
2024-10-31 17:07:21 -0400

    Testing things out

---
+++ src/app/components/Card.tsx
@@ -0,0 +1,7 @@
+export default function Card() {
+    return (
+      <div className="h-[200px] w-[300px] bg-slate-400 drop-shadow-sm">
+        <p>Hello, world.</p>
+      </div>  
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import Image from "next/image";
+import Card from "./components/Card"
 
 export default function Home() {
   return (
     <>
+      <Card />
+      <p>Hello, world.</p>
     </>
   );
 }
