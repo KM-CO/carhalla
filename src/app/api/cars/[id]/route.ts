f0a576baa7a75744d01aa1a144b22261cfadb3f3
mhf29695 <mhf29695@uga.edu>
2024-11-07 16:40:00 -0500

    Added basic MongoDB implementation

---
+++ src/app/api/cars/[id]/route.ts
@@ -0,0 +1,41 @@
+import connectMongoDB from "../../../libs/mongodb";
+import Car from "../../../models/carSchema";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+import mongoose from "mongoose";
+
+interface RouteParams {
+    params: { id: string };
+}
+
+export async function GET(request: NextRequest, { params }: RouteParams) {
+    const { id } = await params;
+    await connectMongoDB();
+    const car = await Car.findOne({ _id: id });
+    return NextResponse.json({ car }, { status: 200 });
+}
+
+export async function PUT(request: NextRequest, { params }: RouteParams) {
+    const { id } = await params;
+    const { car_model: car_model, make: make, price: price, image: image, desc: desc } = await request.json();
+    await connectMongoDB();
+    await Car.findByIdAndUpdate(id, { car_model, make, price, image, desc});
+    return NextResponse.json({ message: "Car updated" }, { status: 200 });
+}
+
+export async function DELETE(request: NextRequest, { params }: RouteParams) {
+    const { id } = await params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return NextResponse.json({ message: "Invalid ID format" }, { status: 400 });
+    }
+
+    await connectMongoDB();
+    const deletedCar = await Car.findByIdAndDelete(id);
+
+    if (!deletedCar) {
+        return NextResponse.json({ message: "Car not found" }, { status: 404 })
+    }
+
+    return NextResponse.json({ message: "Car deleted" }, { status: 200 });
+}
---
+++ src/app/api/users/route.ts
@@ -0,0 +1,27 @@
+import connectMongoDB from "../../libs/mongodb";
+import User from "../../models/userSchema";
+import { NextResponse } from "next/server";
+import { NextRequest } from "next/server";
+
+/** TO DO
+ * Make this secure? Maybe can't request users directly
+ * really just ask PLAs about this or Dr. Stephens
+ */
+export async function GET(request: NextRequest) {
+    // Handle GET requests
+    await connectMongoDB();
+    const users = await User.find();
+    return NextResponse.json({ users });
+}
+
+/** TO DO
+ * figure out encryption
+ * IDK, just make this not sketchy
+ */
+export async function POST(request: NextRequest) {
+    // Handle POST requests
+    const { username } = await request.json();
+    await connectMongoDB();
+    await User.create({ username });
+    return NextResponse.json({ message: "User added successfully" }, { status: 201 });
+}
