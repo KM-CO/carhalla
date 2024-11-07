6394e27564c29ea716124bafe32d0e4656a14eb3
mhf29695 <mhf29695@uga.edu>
2024-11-07 14:42:43 -0500

    Fixed some resizing and CSS issues on Card and Home

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -17,12 +17,12 @@ interface CardProps {
  */
 export default function Card({ price, img, alt, desc }: CardProps) {
   return (
-    <div className="relative flex flex-col h-[300px] min-w-[300px] bg-slate-500 m-4 content-center rounded-3xl border-2 border-gray-500 hover:border-">
+    <div className="mx-auto relative flex flex-grow-0 flex-col h-[300px] min-w-[300px] max-w-[300px] bg-slate-500 content-center rounded-3xl border-2 border-gray-500">
       <div className="absolute z-10 m-2 border-2 rounded-full p-2 min-w-[100px] text-center bg-white drop-shadow-[0_5px_5px_rgba(0,0,0,0.25)] flex-1">${price}</div>
       <div className="relative m-auto mb-0 content-center">
         <div className="absolute h-[200px] w-[250px] duration-1000 opacity-0 hover:opacity-100">
-          <div className="absolute w-[250px] h-[200px] rounded-3xl bg-[rgb(255,255,255,.5)] opacity-0 hover:opacity-100 duration-200">
-            <div className="absolute top-[100px] max-h-[100px] h-[100px] px-2 max-w-[250px] w-[250px] overflow-hidden after:bg-gradient-to-t after:from-white after:h-full after:w-full after:content-[''] after:absolute after:bottom-0 after:left-0 rounded-3xl after:pointer-events-none">
+          <div className="absolute w-[250px] h-[200px] rounded-3xl bg-[rgb(255,255,255,.5)] opacity-0 hover:opacity-100 duration-200 border-2 border-slate-600">
+            <div className="absolute top-[100px] max-h-[100px] h-[98px] px-2 max-w-[250px] w-[246px] line-clamp-4 after:bg-gradient-to-t after:from-white after:via-transparent after:to-transparent after:h-[200px] after:w-[250px] after:content-[''] after:absolute after:bottom-0 after:-left-[2px] rounded-b-3xl after:pointer-events-none after:border-slate-600 after:border-2 after:rounded-b-3xl">
               {desc}
             </div>
           </div>
