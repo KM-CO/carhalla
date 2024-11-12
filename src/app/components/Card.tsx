35babdc84a8206bc15810ab2a74cba71b4fad5ff
Om Patel <otp15001@uga.edu>
2024-11-12 18:29:57 -0500

    Updated the form submission styling and card styling.

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -4,6 +4,7 @@ import Button from "./Button"
 import Image from 'next/image'
 import { LoggedStatus } from "./Contexts";
 import { useContext } from "react";
+import styles from './Card.module.css';
 
 interface CardProps {
   model: string,
@@ -21,22 +22,35 @@ interface CardProps {
  * Change button stuff to EDIT when logged in (should be something about it on slides using ternary operators)
  */
 export default function Card({ model, make, price, img, alt, desc }: CardProps, key: string) {
-  const loggedInStatus = useContext(LoggedStatus)
+  const loggedInStatus = useContext(LoggedStatus);
   return (
-    <div className="mx-auto relative flex flex-grow-0 flex-col h-[300px] min-w-[300px] max-w-[300px] bg-slate-500 content-center rounded-3xl border-2 border-gray-500">
-      <div className="absolute z-10 m-2 border-2 rounded-full p-2 min-w-[100px] text-center bg-white drop-shadow-[0_5px_5px_rgba(0,0,0,0.25)] flex-1">${price}</div>
-      <div className="relative m-auto mb-0 content-center">
-        <div className="absolute h-[200px] w-[250px] duration-1000 opacity-0 hover:opacity-100">
-          <div className="absolute grid grid-rows-2 w-[250px] h-[200px] rounded-3xl bg-[rgb(255,255,255,.5)] opacity-0 hover:opacity-100 duration-200 border-2 border-slate-600">
-          <div className="justify-self-center self-end text-xl">{model} <b>{make}</b></div>
-            <div className="absolute top-[100px] max-h-[100px] h-[98px] px-2 max-w-[250px] w-[246px] line-clamp-4 after:bg-gradient-to-t after:from-white after:via-transparent after:to-transparent after:h-[200px] after:w-[250px] after:content-[''] after:absolute after:bottom-0 after:-left-[2px] rounded-b-3xl after:pointer-events-none after:border-slate-600 after:border-2 after:rounded-b-3xl">
-              {desc}
-            </div>
-          </div>
+    <div className={styles.card}>
+      <div className={styles['card-price']}>${price}</div>
+      <div className={styles['card-image-container']}>
+        <Image
+          height={200}
+          width={250}
+          src={img}
+          alt={alt}
+          className={styles['card-image']}
+          priority
+        />
+        <div className={styles['card-info-overlay']}>
+          <div className={styles['card-model-make']}>{model} <b>{make}</b></div>
+          <div className={styles['card-desc']}>{desc}</div>
         </div>
-        <Image height={200} width={250} src={img} alt={alt} className="min-h-[200px] min-w-[250px] max-h-[200px] max-w-[250px] resize-none rounded-3xl border-gray-600 border-2" priority />
       </div>
-      {loggedInStatus ? <Link href={ { pathname:"form-submission"/* + `/${key}` */, query: { model: model, make: make, price: price, img: img, alt: alt, desc: desc, loggedIn: loggedInStatus } } }  className="m-auto"><Button onClick={() => {}}>Edit</Button></Link> : <Link href={ { pathname:"view"/* + `/${key}` */, query: { model: model, make: make, price: price, img: img, alt: alt, desc: desc, loggedIn: loggedInStatus } } } className="m-auto"><Button onClick={() => {}}>Buy</Button></Link>}
+      <div className={styles['card-button-container']}>
+        {loggedInStatus ? (
+          <Link href={{ pathname: "form-submission", query: { model, make, price, img, alt, desc, loggedIn: loggedInStatus } }}>
+            <Button onClick={() => {}}>Edit</Button>
+          </Link>
+        ) : (
+          <Link href={{ pathname: "view", query: { model, make, price, img, alt, desc, loggedIn: loggedInStatus } }}>
+            <Button onClick={() => {}}>Buy</Button>
+          </Link>
+        )}
+      </div>
     </div>
   );
 }
--- src/app/form-submission/page.tsx
+++ src/app/form-submission/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 import Link from "next/link";
 import { useSearchParams } from 'next/navigation'
 import { useState } from "react";
-
+import styles from './page.module.css';
 export default function Page() {
 
     const searchParams = useSearchParams();
@@ -30,30 +30,75 @@ export default function Page() {
      * fix appearance when resizing
      */
     return (
-        <form className="grid grid-rows-[min-content] h-full">
-            <div className="grid grid-cols-2 grid-rows-2">
-                <Link href={ { pathname:"/", query: { loggedIn: loggedInStatus } } } className="absolute right-5 my-4"><div className="right-5 absolute h-[30px] w-[30px] after:content-[''] after:right-[14px] before:right-[14px] after:h-[30px] after:border-[1px] after:border-black after:bg-black before:bg-black after:rotate-45 after:absolute before:content-[''] before:h-[30px] before:border-[1px] before:border-black before:-rotate-45 before:absolute"></div></Link>
-                <div className="flex flex-col items-center justify-evenly">
-                    <Image height={200} width={250} src={initialImg || "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ="} alt={"no image"} className="min-h-[200px] min-w-[250px] max-h-[200px] max-w-[250px] resize-none rounded-3xl border-gray-600 border-2" priority />
-                    <div className="flex w-[300px] p-2 bg-slate-600 justify-center">
-                        <span>https://</span>
-                        <input className="w-[100%] p-1" placeholder="Image link" value={img || ""} onChange={(e) => setImg(e.target.value)}/>
-                    </div>
-                </div>
-                <div className="flex flex-col items-center justify-evenly">
-                    <div className="flex w-[300px] p-2 bg-slate-600 justify-center">
-                        <input className="w-[100%] p-1" placeholder="Model" value={model || ""} onChange={(e) => setModel(e.target.value)} />
-                    </div><div className="flex w-[300px] p-2 bg-slate-600 justify-center">
-                        <input className="w-[100%] p-1" placeholder="Make" value={make || ""} onChange={(e) => setMake(e.target.value)} />
-                    </div><div className="flex w-[300px] p-2 bg-slate-600 justify-center">
-                        <input className="w-[100%] p-1" placeholder="Price" value={price || ""} onChange={(e) => setPrice(e.target.value)} />
-                    </div>
-                </div>
-                <div className="col-span-2 flex flex-col justify-center">
-                    <textarea rows={8} className="mx-auto w-[90%] resize-none border-2 border-slate-400 p-2" value={desc || ""} onChange={(e) => setDesc(e.target.value)} />
-                    <Link href={ { pathname: "", query: { model: model, make: make, price: price, img: img, alt: "", desc: desc, loggedIn: loggedInStatus } } } className="m-auto"><Button onClick={addCarHandler} onSubmit={(e) => e.preventDefault()}>Submit</Button></Link>
+        <div className={styles.formContainer}>
+            <Link href={{ pathname: "/", query: { loggedIn: loggedInStatus } }}>
+                <div className={styles.linkIcon}></div>
+            </Link>
+            <div className={styles.imageContainer}>
+                <Image
+                    height={200}
+                    width={250}
+                    src={initialImg || "https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ="}
+                    alt={"no image"}
+                    className={styles.image}
+                    priority
+                />
+                <div className={styles.inputFieldContainer}>
+                    <span>https://</span>
+                    <input
+                        className={styles.inputField}
+                        placeholder="Image link"
+                        value={img || ""}
+                        onChange={(e) => setImg(e.target.value)}
+                    />
                 </div>
             </div>
-        </form>
+            <div className={styles.formFieldsContainer}>
+                <div className={styles.inputFieldContainer}>
+                    <input
+                        className={styles.inputField}
+                        placeholder="Model"
+                        value={model || ""}
+                        onChange={(e) => setModel(e.target.value)}
+                    />
+                </div>
+                <div className={styles.inputFieldContainer}>
+                    <input
+                        className={styles.inputField}
+                        placeholder="Make"
+                        value={make || ""}
+                        onChange={(e) => setMake(e.target.value)}
+                    />
+                </div>
+                <div className={styles.inputFieldContainer}>
+                    <input
+                        className={styles.inputField}
+                        placeholder="Price"
+                        value={price || ""}
+                        onChange={(e) => setPrice(e.target.value)}
+                    />
+                </div>
+            </div>
+            <div className={styles.textareaContainer}>
+                <textarea
+                    rows={8}
+                    className={styles.textareaField}
+                    placeholder="Description"
+                    value={desc || ""}
+                    onChange={(e) => setDesc(e.target.value)}
+                />
+            </div>
+            <div className={styles.buttonContainer}>
+                <Button
+                    onClick={addCarHandler}
+                    onSubmit={(e) => e.preventDefault()}
+                    //className={styles.button}
+                >
+                    Submit
+                </Button>
+            </div>
+            {/* Close button to go back */}
+            <button className={styles.closeButton} onClick={() => window.history.back()}>Back</button>
+        </div>
     );
 }
