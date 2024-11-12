b57ab1a144f06ebe4c9c9de44b34f58cfa8c3012
mhf29695 <mhf29695@uga.edu>
2024-11-12 14:30:30 -0500

    Added PUT to form-submission/edit.

--- src/app/form-submission/add/page.tsx
+++ src/app/form-submission/add/page.tsx
@@ -32,7 +32,7 @@ export default function Page() {
                 throw new Error('Network response was not ok')
             }
 
-            setImg("");
+            setImg(initialImg);
             setMake("");
             setCarModel("");
             setPrice(0);
@@ -54,7 +54,7 @@ export default function Page() {
             <div className="grid grid-cols-2 grid-rows-2">
                 <Link href="/" className="absolute right-5 my-4"><div className="right-5 absolute h-[30px] w-[30px] after:content-[''] after:right-[14px] before:right-[14px] after:h-[30px] after:border-[1px] after:border-black after:bg-black before:bg-black after:rotate-45 after:absolute before:content-[''] before:h-[30px] before:border-[1px] before:border-black before:-rotate-45 before:absolute"></div></Link>
                 <div className="flex flex-col items-center justify-evenly">
-                    <Image height={200} width={250} src={img || initialImg} alt={""} className="min-h-[200px] min-w-[250px] max-h-[200px] max-w-[250px] resize-none rounded-3xl border-gray-600 border-2" priority />
+                    <Image height={200} width={250} src={img === "" ?  initialImg : img} alt={car_model + " " + make} className="min-h-[200px] min-w-[250px] max-h-[200px] max-w-[250px] resize-none rounded-3xl border-gray-600 border-2" priority />
                     <div className="flex w-[300px] p-2 bg-slate-600 justify-center">
                         <input className="w-[100%] p-1" placeholder="Image link" value={img || ""} onChange={(e) => setImg(e.target.value)}/>
                     </div>
@@ -70,7 +70,7 @@ export default function Page() {
                 </div>
                 <div className="col-span-2 flex flex-col justify-center">
                     <textarea rows={8} className="mx-auto w-[90%] resize-none border-2 border-slate-400 p-2" value={desc || ""} onChange={(e) => setDesc(e.target.value)} />
-                    <button type="submit">Submit</button>
+                    <Button>Submit</Button>
                 </div>
             </div>
         </form>
