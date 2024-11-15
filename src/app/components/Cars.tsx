8ed1d79e097f03def07b43cfd11a8ffca107369f
mhf29695 <mhf29695@uga.edu>
2024-11-14 23:10:40 -0500

    Fixed some CSS

--- src/app/components/Cars.tsx
+++ src/app/components/Cars.tsx
@@ -31,11 +31,11 @@ export default function Cars() {
     }, []);
 
     return (
-        <div className="m-3 grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-3" >
+        <div className="m-3 grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] gap-3 items-center" >
             {cars.map((car: Car) => (
                 <Card key={car._id} id={car._id} model={car.car_model} make={car.make} price={car.price} desc={car.desc} img={car.img} alt={car.car_model + " " + car.make} />
             ))}
-            <div className="flex border-3 border-neutral-500 items-center hover:border-neutral-700 align-middle justify-center h-[280px] w-[300px] m-auto">
+            <div className="flex border-3 border-neutral-500 items-center hover:border-neutral-700 justify-center h-[280px] w-[300px] m-auto">
                 <Link href="form-submission/add" className="text-9xl text-neutral-500 hover:text-neutral-700">+</Link>
             </div>
         </div>
